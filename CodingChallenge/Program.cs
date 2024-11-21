c4e3573e7ded0a215b992d3828bf400ad4522bbf
itamaredbmaestro <79922864+itamaredbmaestro@users.noreply.github.com>
2024-11-21 18:05:35 +0200

    Init

---
+++ CodingChallenge/Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello World");
+
+// Using the attached files:
+
+// 1. Print the avarage for each student
+
+// 2. Print the avarage for each professor
+
+// 3. For each subject, Print the professor with the heigher grades
